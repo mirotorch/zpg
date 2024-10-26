a9b6699d73df771648c981450d9ef90a2f5b4b23
Ruslan Kovalenko <rus.kovalenko.kharkov@gmail.com>
2024-10-26 10:01:42 +0200

    Phong and Blinn-Phong shading

--- shaders/phong_v.glsl
+++ shaders/phong_v.glsl
@@ -1,17 +1,20 @@
-#version 330 core
-layout(location=0) in vec3 vp;
-layout(location=1) in vec3 vn;
+#version 400
+layout(location=0) in vec3 vp;  
+layout(location=1) in vec3 vn;  
 
 uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
 
-out vec3 worldNorm;
-out vec3 worldPos;
+out vec3 fragPos;
+out vec3 normal; 
 
 void main() 
 {
-    worldPos = vp;
-    worldNorm = vn;
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(vp, 1.0);
-};
+    fragPos = vec3(modelMatrix * vec4(vp, 1.0));
+    
+    mat3 normalMatrix = transpose(inverse(mat3(modelMatrix)));
+    normal = normalize(normalMatrix * vn);
+
+    gl_Position = projectionMatrix * viewMatrix * vec4(fragPos, 1.0);
+}
