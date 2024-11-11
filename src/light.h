c6c652377b492868fb97d1329128134720f65d62
Ruslan Kovalenko <rus.kovalenko.kharkov@gmail.com>
2024-11-11 15:35:52 +0100

    multiple lights files

---
+++ src/light.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <glm/vec3.hpp>
+
+#define NUM_LIGHTS 4
+
+struct Light {
+    glm::vec3 position;
+    glm::vec3 color;
+
+    float constant;
+    float linear;
+    float quadratic;
+};
